39a8afcc38878edad6f5eca33e963258ba1e4093
Ajeesh <ajeesh.aj@gmail.com>
2025-04-28 10:59:58 +0200

    Remove reference to confirmed.html for clean deployment

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,6 @@ export default defineConfig({
         index: path.resolve(__dirname, 'index.html'),
         login: path.resolve(__dirname, 'public/pages/login.html'),
         signup: path.resolve(__dirname, 'public/pages/signup.html'),
-        confirmed: path.resolve(__dirname, 'public/pages/confirmed.html'),
         main: path.resolve(__dirname, 'public/pages/main.html'),
         // keep adding more if needed
       }
