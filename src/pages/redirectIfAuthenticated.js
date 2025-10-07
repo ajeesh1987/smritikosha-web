12270428f56c7c6049530eac6ddca4e35212a744
Ajeesh <ajeesh.aj@gmail.com>
2025-10-07 09:08:20 +0200

    fix redirect issue

--- src/pages/redirectIfAuthenticated.js
+++ src/pages/redirectIfAuthenticated.js
@@ -4,6 +4,6 @@ import { supabase } from '../../lib/supabaseClient.js';
 export async function redirectIfAuthenticated() {
   const { data: { session } } = await supabase.auth.getSession();
   if (session?.user) {
-    window.location.href = '/pages/main.html';
+    window.location.href = '/main.html';
   }
 }
